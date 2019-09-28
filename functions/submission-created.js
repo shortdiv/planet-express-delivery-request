ca857dad751224171b069c8bbb1158258626d15a
Divya Sasidharan <hello@shortdiv.com>
2019-09-28 10:29:58 -0500

    add fauna

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,9 +1,29 @@
 /* Triggered when a form submission is posted to your site. */
+const faunadb = require("faunadb");
+const q = faunadb.query;
+
+const client = new faunadb.Client({ secret: process.env.VUE_APP_FAUNA_SECRET });
+
 exports.handler = (event, context, callback) => {
   const body = JSON.parse(event.body).payload;
-  console.log(body);
-  return callback(null, {
-    statusCode: 200,
-    body: JSON.stringify(body)
-  });
+
+  client
+    .query(
+      q.Create(q.Class("deliveries"), {
+        data: body
+      })
+    )
+    .then(ret => {
+      console.log(ret);
+      return callback(null, {
+        statusCode: 200,
+        body: JSON.stringify(body)
+      });
+    })
+    .catch(err => {
+      return callback(null, {
+        statusCode: 400,
+        body: JSON.stringify(err)
+      });
+    });
 };
